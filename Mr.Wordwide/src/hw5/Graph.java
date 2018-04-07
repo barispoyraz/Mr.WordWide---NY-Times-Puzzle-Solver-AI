f30c92a4dccd26e0041573154ce8f0f0c9ff801d
cgdsyilmaz <cagdasyilmaz96@gmail.com>
2018-04-07 17:15:16 +0300

    First part - a

--- Mr.Wordwide/src/hw5/Graph.java
+++ Mr.Wordwide/src/hw5/Graph.java
@@ -45,7 +45,7 @@ class Graph
  
     // The function to do Topological Sort. It uses
     // recursive topologicalSortUtil()
-    void topologicalSort()
+    Stack topologicalSort()
     {
         Stack stack = new Stack();
  
@@ -61,25 +61,70 @@ class Graph
             if (visited[i] == false)
                 topologicalSortUtil(i, visited, stack);
  
-        // Print contents of stack
-        while (stack.empty()==false)
-            System.out.print(stack.pop() + " ");
+        return stack;
+        
     }
  
     // Driver method
     public static void main(String args[])
     {
         // Create a graph given in the above diagram
-        Graph g = new Graph(6);
-        g.addEdge(5, 2);
-        g.addEdge(5, 0);
-        g.addEdge(4, 0);
-        g.addEdge(4, 1);
-        g.addEdge(2, 3);
-        g.addEdge(3, 1);
- 
+        Graph g1_a = new Graph(8);
+        Map m1_a = new HashMap();
+        Stack s1_a;
+        m1_a.put(0, "Crazy");
+        m1_a.put(1, "Professors");
+        m1_a.put(2, "Hackers");
+        m1_a.put(3, "Eccentrics");
+        m1_a.put(4, "Teachers");
+        m1_a.put(5, "Programmers");
+        m1_a.put(6, "Dwarfs");
+        m1_a.put(7, "Everything");
+        
+        g1_a.addEdge(0, 1);
+        g1_a.addEdge(0, 2);
+        g1_a.addEdge(1, 3);
+        g1_a.addEdge(1, 4);
+        g1_a.addEdge(2, 3);
+        g1_a.addEdge(2, 5);
+        g1_a.addEdge(3, 6);
+        g1_a.addEdge(4, 6);
+        g1_a.addEdge(5, 6);
+        g1_a.addEdge(6, 7);
+//        g1.addEdge(13, 9);
+//        g1.addEdge(13, 11);
+//        g1.addEdge(13, 12);
+//        g1.addEdge(11, 9);
+//        g1.addEdge(11, 10);
+//        g1.addEdge(12, 9);
+//        g1.addEdge(12, 10);
+//        g1.addEdge(9, 7);
+//        g1.addEdge(10, 7);
+        
+        System.out.println("-------------------------------------------------");
         System.out.println("Following is a Topological " +
-                           "sort of the given graph");
-        g.topologicalSort();
+                           "sort of the test data one - Crazy:");
+        s1_a = g1_a.topologicalSort();
+        while (s1_a.empty() == false)
+            System.out.println(m1_a.get(s1_a.pop()));
+        
+        
+        System.out.println("-------------------------------------------------");
+        System.out.println("Following is a Topological " +
+                           "sort of the test data one - Jacque:");
+        
+        
+        System.out.println("-------------------------------------------------");
+        System.out.println("Following is a Topological " +
+                           "sort of the test data two - Num:");
+        
+        
+        System.out.println("-------------------------------------------------");
+        System.out.println("Following is a Topological " +
+                           "sort of the test data two - Ord:");
+        
+        System.out.println("-------------------------------------------------");
+        System.out.println("Following is a Topological " +
+                           "sort of the test data three - Blimpy:");
     }
 }
